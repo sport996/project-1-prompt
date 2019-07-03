bfa9383027ef6f90d63645649b092b136dbb4bdd
Mahdi Albahrani <bosalih@Albahranis-MacBook-Air.local>
2019-07-03 10:01:57 +0300

    Ahmed Albahrani project Tues v2

--- script.js
+++ script.js
@@ -1,124 +1,111 @@
 //DOM Manipulation
-//Getting Players Names
+//Getting Players Names and saving them in variables
 const player1 = prompt('Enter Player 1 name:');
 const player2 = prompt('Enter Player 2 name:');
-// let getPlayersName = $('header h3');
+//Changing the style of players names by jQuery library
 $('h3#p1').css('fontSize', '35px');
 $('h3#p2').css('fontSize', '35px');
+//Getting the values into the DOM(Document Object Model) Tree
 $('h3#p1').text(player1.toUpperCase());
 $('h3#p2').text(player2.toUpperCase());
-
+//Initiating the value of symbol which will be entered first
 let symbol = "X";
+//Getting the symbol into the box to clarify who's turn it is
 $('span > h1').text(symbol);
+//Changing the style of the box contents by jQuery library
 $('span > h1').css({
     'fontSize':'80px','color':'yellow'
 });
-
+//Initiating the event type
 const eventType = 'click';
+//Targeting the table data by its selector in jQuery library
+//Which will give us an array of our table date(9 indexes)
 const targetBox = $('td.box');
 // const targetBox = document.querySelectorAll('.box');
-
+//The callback function which specifies what will happen if 
+//our event has happened
 const callback = function(){
-    // console.log('Clicked box'); // To test the event
-    // we use event.target to catch the element that triggered the event
-    $(event.target).text(symbol);
-    $(event.target).css({'fontSize':'90px', 'color':'#2196F3'});
-    turns();
-    $('span > h1').text(symbol);
-    let result;
+    // To test the event
+    // console.log('Clicked box');
 
+    // I used event.target to catch the element that triggered the event
+    //And input the value of symbol variable in it
+    $(event.target).text(symbol);
+    //changing the style of the input value
+    $(event.target).css({'fontSize':'90px', 'color':'#2196F3'});
+    //Method Turn, used to toggle the value of our variable symbol,
+    //from X to O and vice versa
+    turns();
+    //Updating the value in the box that specifies the turns
+    $('span > h1').text(symbol);
+    //Basically, what follows is if-else conditions to specify
+    //the winner, depending on the return of function winner()
     if (winner() == 'X'){
+        //putting the value of turns box to dash, since the game
+        //has ended
         $('span > h1').text('_');
+        //Turning off the event Listener on jQuery
         $(targetBox).off('click');
+        //Styling the result by jQuery
         $('aside > p').css('border','double #2196F3');
+        //The result text
         $('aside > p').text("Player 1 has won");
-        // alert("Player 1 has won");
     } 
     else if (winner() == 'O'){
         $('span > h1').text('_');
         $(targetBox).off('click');
         $('aside > p').css('border','double #2196F3');
         $('aside > p').text("Player 2 has won");
-        // alert("Player 2 has won");
     }
+    //Checking the value of our array
+    //when the array is full and non of the players has won
+    //the condition will return Tie result
     else if ($(targetBox).text().length === 9){
         $('span > h1').text('_');
         $('aside > p').css('border','double #2196F3');
         $('aside > p').text("It is a Tie ^_^");
-        // alert("It is a Tie ^_^");
     }
     
 }
-
+//Function check(num1,num2,num3) will be used in our winner() function
+//it takes 3 arguments, checks if they are empty, if they are empty
+//it will check if they are equal and return true
+//otherwise it will return false
 function check(num1,num2,num3){
     if( (num1 && num2 && num3 ) != '' )
             if(num1 == num2 && num2 == num3)
                 return true;
 }
-
+//
 function winner() { 
-// console.log(targetBox);
-// console.log($(targetBox[0]).text());
-    //For catching the wins by Rows
-    for(let i=0;i<=2;i++){ //Columns
+    //For catching the wins
+    //Columns
+    for(let i=0;i<=2;i++){ 
         if(check($(targetBox[i]).text(), $(targetBox[i+3]).text(), $(targetBox[i+6]).text()))
             return $(targetBox[i]).text();
-        // if( ($(targetBox[i]).text() && $(targetBox[i+3]).text() && $(targetBox[i+6]).text() ) != '' )
-        //     if($(targetBox[i]).text() == $(targetBox[i+3]).text() && $(targetBox[i+3]).text() == $(targetBox[i+6]).text())
     }
-    for(let i=0;i<=6;i+=3){ //Rows 
+    //Rows
+    for(let i=0;i<=6;i+=3){  
         if(check($(targetBox[i]).text(), $(targetBox[i+1]).text(), $(targetBox[i+2]).text()))
             return $(targetBox[i]).text();
-        // if( ($(targetBox[i]).text() && $(targetBox[i+1]).text() && $(targetBox[i+2]).text() ) != '' )
-        //     if($(targetBox[i]).text() == $(targetBox[i+1]).text() && $(targetBox[i+1]).text() == $(targetBox[i+2]).text()) 
     }
     //Corners
     if(check($(targetBox[0]).text(), $(targetBox[4]).text(), $(targetBox[8]).text()))
         return $(targetBox[0]).text();
     else if(check($(targetBox[2]).text(), $(targetBox[4]).text(), $(targetBox[6]).text()))
         return $(targetBox[2]).text();
-    // for(let i=6;i<=8;i++){//Third Row 
-    //     if( ($(targetBox[i]).text() && $(targetBox[i+1]).text() && $(targetBox[i+2]).text() ) != '' )
-    //         if($(targetBox[i]).text() == $(targetBox[i+1]).text() && $(targetBox[i+1]).text() == $(targetBox[i+2]).text()) 
-    //             return $(targetBox[6]).text();
-    // }
-    // //For catching the wins by Columns
-    // for(let i=0;i<=6;i+=3){//First Column 
-    //     if( ($(targetBox[i]).text() && $(targetBox[i+3]).text() && $(targetBox[i+6]).text() ) != '' )
-    //         if($(targetBox[i]).text() == $(targetBox[i+3]).text() && $(targetBox[i+3]).text() == $(targetBox[i+6]).text()) 
-    //             return $(targetBox[0]).text();
-    //     }
-    // for(let i=1;i<=7;i+=3){//Second Column 
-    //     if( ($(targetBox[i]).text() && $(targetBox[i+3]).text() && $(targetBox[i+6]).text() ) != '' )
-    //         if($(targetBox[i]).text() == $(targetBox[i+3]).text() && $(targetBox[i+3]).text() == $(targetBox[i+6]).text()) 
-    //             return $(targetBox[1]).text();
-    // }
-    // for(let i=2;i<=8;i+=3){//Third Column 
-    //     if( ($(targetBox[i]).text() && $(targetBox[i+3]).text() && $(targetBox[i+6]).text() ) != '' )
-    //         if($(targetBox[i]).text() == $(targetBox[i+3]).text() && $(targetBox[i+3]).text() == $(targetBox[i+6]).text()) 
-    //             return $(targetBox[2]).text();
-    //     }
-    // //For catching the wins by Corners 
-    // for(let i=0;i<=8;i+=4){//First Corner index from 0
-    //     if( ($(targetBox[i]).text() && $(targetBox[i+4]).text() && $(targetBox[i+8]).text() ) != '' )
-    //         if($(targetBox[i]).text() == $(targetBox[i+4]).text() && $(targetBox[i+4]).text() == $(targetBox[i+8]).text()) 
-    //             return $(targetBox[0]).text();
-    //     }
-    // for(let i=2;i<=6;i+=2){//Second Corner from index 2
-    //     if( ($(targetBox[i]).text() && $(targetBox[i+2]).text() && $(targetBox[i+4]).text() ) != '' )
-    //         if($(targetBox[i]).text() == $(targetBox[i+2]).text() && $(targetBox[i+2]).text() == $(targetBox[i+4]).text()) 
-    //             return $(targetBox[2]).text();
-    //     }
 }
+//Function turns(), checks if the value of variable symbol is X it 
+//change it to O and vise versa
 function turns() {
-
     if (symbol === "X") {
         symbol = "O";
     } else {
         symbol = "X";
     }
 }
-
+//Binding the table data boxes to the callback function ..
+//of an event type that is stored in eventType Variable('click')
+//by using a jQuery method called one()
+//which activates the callback function only once for each 
 targetBox.one(eventType, callback);
-// document.addEventListener(eventType,targetBox);
-
